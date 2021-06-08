67e78aaf2eab0e3debeb536f4e062c24a3c2eba3
BJ Anderson <andersonb79@gmail.com>
2021-06-08 06:50:02 -0400

    added more to html posts

--- html/js/posts.js
+++ html/js/posts.js
@@ -6,7 +6,7 @@ function showPosts() {
   postsDiv.innerHTML = ''
 
   const posts = getPosts()
-  const postsToDisplay = posts.map((post) => getPostDisplay(post))
+  const postsToDisplay = posts.map((post) => getPostDisplayUsingTemplate(post))
   postsToDisplay.forEach((post) => {
     postsDiv.appendChild(post)
   })
@@ -18,51 +18,83 @@ function getPosts() {
       author: 'User One',
       title: 'An Interesting Post',
       content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      createdAt: new Date('9/1/2021')
+      createdAt: new Date('9/1/2021'),
+      likes: 1
     },
     {
       author: 'User Two',
       title: 'Another Interesting Post',
       content: 'Nulla facilisi cras fermentum odio eu feugiat pretium nibh ipsum. Turpis cursus in hac habitasse platea dictumst quisque sagittis. Sed egestas egestas fringilla phasellus. Tincidunt dui ut ornare lectus sit. Sit amet est placerat in egestas erat imperdiet sed euismod. Turpis in eu mi bibendum neque egestas. Integer feugiat scelerisque varius morbi enim nunc faucibus a pellentesque. Ut ornare lectus sit amet est placerat in egestas erat. Ultricies tristique nulla aliquet enim tortor at. Mauris a diam maecenas sed enim ut sem. Odio morbi quis commodo odio aenean sed adipiscing diam donec.',
-      createdAt: new Date('9/2/2021')
+      createdAt: new Date('9/2/2021'),
+      likes: 2
     }
   ]
   return posts
 }
 
-function getPostDisplay(post) {
-  // const postDiv = document.createElement('div')
-  // postDiv.className = 'post'
-  // const titleDiv = document.createElement('div')
-  // titleDiv.className = 'post-title'
-  // titleDiv.innerText = post.title
-  // postDiv.appendChild(titleDiv)
+/**
+ * This function uses a pre-made template that lives in the html document.
+ * Using a template is easier to maintain and write css for than using pure JS to create the dom elements.
+ */
+function getPostDisplayUsingTemplate(post) {
+  const postTemplate = document.querySelector('#posttemplate').content.cloneNode(true)
 
-  // const authorDiv = document.createElement('div')
-  // authorDiv.className = 'post-author'
-  // authorDiv.innerText = `${post.author} - ${post.createdAt.toLocaleDateString()}`
-  // postDiv.appendChild(authorDiv)
-
-  // const contentDiv = document.createElement('div')
-  // contentDiv.className = 'post-content'
-  // contentDiv.innerText = post.content
-  // postDiv.appendChild(contentDiv)
-  // return postDiv
-
-  const postTemplate = document.querySelector('#posttemplate')
-  const clone = postTemplate.content.cloneNode(true)
-
-  const title = clone.querySelector('.post-title')
+  const title = postTemplate.querySelector('.post-title')
   title.innerText = post.title
 
-  const authorName = clone.querySelector('.post-author-name')
-  authorName.innerText = post.author
+  const author = postTemplate.querySelector('.post-author')
+  author.innerText = post.author
 
-  const authorDate = clone.querySelector('.post-author-date')
-  authorDate.innerText = post.createdAt.toLocaleDateString()
+  const date = postTemplate.querySelector('.post-date')
+  date.innerText = post.createdAt.toLocaleDateString()
 
-  const content = clone.querySelector('.post-content')
+  const likes = postTemplate.querySelector('.post-likes')
+  likes.append(post.likes)
+
+  const content = postTemplate.querySelector('.post-content')
   content.innerText = post.content
 
-  return clone
+  return postTemplate
+}
+
+/**
+ * This function does basically the same thing as getPostDisplayUsingTemplate,
+ * except it does it purely in JavaScript and does not use a pre-made html template.
+ */
+function getPostDisplayWithoutUsingTemplate(post) {
+  const postDiv = document.createElement('div')
+  postDiv.className = 'post'
+  const titleDiv = document.createElement('div')
+  titleDiv.className = 'post-title'
+  titleDiv.innerText = post.title
+  postDiv.appendChild(titleDiv)
+
+  const postInfoDiv = document.createElement('div')
+  postInfoDiv.className = 'post-info'
+
+  const authorSpan = document.createElement('span')
+  authorSpan.className = 'post-author'
+  authorSpan.innerText = post.author
+
+  const dateSpan = document.createElement('span')
+  dateSpan.className = 'post-date'
+  dateSpan.innerText = post.createdAt.toLocaleDateString()
+
+  const likesSpan = document.createElement('span')
+  likesSpan.className = 'post-likes'
+  likesSpan.innerText = `Likes: ${post.likes}`
+
+  postInfoDiv.appendChild(authorSpan)
+  postInfoDiv.append(' - ')
+  postInfoDiv.appendChild(dateSpan)
+  postInfoDiv.append(' - ')
+  postInfoDiv.appendChild(likesSpan)
+
+  postDiv.appendChild(postInfoDiv)
+
+  const contentDiv = document.createElement('div')
+  contentDiv.className = 'post-content'
+  contentDiv.innerText = post.content
+  postDiv.appendChild(contentDiv)
+  return postDiv
 }
